a810347a15cc186dde3d4b43f6fd9ca63460207c
Micah Sargent <msargen@ncsu.edu>
2023-07-27 22:54:24 -0400

    Moving structure back because it should work
    - It seems to work like this for the test pokémon sim

---
+++ src/webapp/nameanator.js
@@ -0,0 +1,7 @@
+function main () {
+    alert("You pressed the button, how dare you...");
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed, you must be a wizard!";
+}
