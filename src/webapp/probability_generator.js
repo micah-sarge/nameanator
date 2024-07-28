93f1dc23f415ec5d310b1358ace9cca37b3bf1f3
Micah Sargent <msargen@ncsu.edu>
2024-07-28 09:19:28 -0400

    Add basic css styling

--- src/webapp/probability_generator.js
+++ src/webapp/probability_generator.js
@@ -1,7 +1,3 @@
 function main () {
-    alert("You pressed the button, how dare you...you monster");
-}
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "How did you do that? You must be a wizard!";
+    alert("Patience Iago, there is nothing to submit at this point. ");
 }
