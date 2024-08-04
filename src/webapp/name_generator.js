0c6450aad160d7ade044ee0277b8d3fb35ecc582
Scheills <50057051+Scheills@users.noreply.github.com>
2024-08-04 09:50:03 -0400

    Testerson (#5)
    * Added base algorithm for converting names to arrays
    
    * Added base algorithm for converting names to arrays
    
    * added base code for generator algorithm

--- src/webapp/name_generator.js
+++ src/webapp/name_generator.js
@@ -26,3 +26,81 @@ function clearList() {
     var nameList = document.getElementById("nameList");
     nameList.innerHTML = "";
 }
+
+
+
+
+
+//if random needs init, use systime
+
+var countingNumber = 0, arraySum = 0;
+var theName = "John";
+
+//sum the starting letter array to use for next calculation
+for (var i=0; i<27; i++) {
+    arraySum += firstLetters[i];
+}
+
+//generate a random number and use the previous sum for mod to get a random number between 1 and the sum
+countingNumber = (random % arraySum) + 1;
+
+//use mod result to count through the array of values to get letter result
+for (var i=0; countingNumber>firstLetters[i]; i++) {
+    countingNumber -= firstLetters[i];
+}
+
+//convert i index to corresponding letter for first letter of name
+theName.charAt(0) = String.fromCharCode(i+97);
+
+//repeat the process of sum, random, and mod
+for (var j=0, arraySum=0; j<27; j++) {
+    arraySum += doubleLetters[i][j];
+}
+
+countingNumber = (random % arraySum) + 1;
+
+//use mod result to count through the array of values using i as first index to get second letter result
+for (var j=0; countingNumber>firstLetters[i][j]; j++) {
+    countingNumber -= firstLetters[i][j];
+}
+
+//convert j index to corresponding letter for second letter of name
+theName.charAt(1) = String.fromCharCode(j+97);
+
+//start a loop to generate letters based on the two previous letters
+var wordEnd=false
+for (var x=2; !wordEnd; x++) {
+    //sum, random, and mod as before
+    for (var k=0, arraySum=0; k<27; k++) {
+        arraySum += doubleLetters[i][j][k];
+    }
+
+    countingNumber = (random % arraySum) + 1;
+
+    //use mod result to count through the array of values using i and j as indices to get third letter result
+    for (var k=0; countingNumber>firstLetters[i][j][k]; k++) {
+        countingNumber -= firstLetters[i][j][k];
+    }
+
+    //convert k index to corresponding letter for next letter based on previous two, if terminating character, use " "
+    if (k<26) {
+        theName.charAt(x) = String.fromCharCode(k+97);
+    } else {
+        theName.charAt(x) = " ";
+        wordEnd = true;
+    }
+
+    //shift second and third letters to first and second to prep for next loop
+    i=j;
+    j=k;
+}
+
+/*
+some kind of function to avoid weird names
+
+importantly, they cannot be too long
+
+also they cannot be real (check input list)
+
+print.theName
+*/
--- src/webapp/probability_generator.js
+++ src/webapp/probability_generator.js
@@ -5,3 +5,77 @@ function main () {
 function myFunction() {
     document.getElementById("demo").innerHTML = "How did you do that? You must be a wizard!";
 }
+
+/*
+if (dataInput==textBox) {
+    Read from text box
+} else if (dataInput==file) {
+    Read from file
+} else {
+    skip to generation
+}
+
+change all letters to lowercase
+
+*/
+
+
+//Initialize the 1D, 2D, and 3D arrays
+var firstLetters = new Array();
+for (var i=0; i<27; i++) {
+    firstLetters[i] = 0;
+}
+
+var doubleLetters = new Array();
+for (var i=0; i<27; i++) {
+    doubleLetters[i] = new Array();
+    for(var j=0; j<27; j++) {
+        doubleLetters[i][j] = 0;
+    }
+}
+
+var tripleLetters = new Array();
+for (var i=0; i<27; i++) {
+    tripleLetters[i] = new Array();
+    for(var j=0; j<27; j++) {
+        tripleLetters[i][j] = new Array();
+        for(var k=0; k<27; k++) {
+            tripleLetters[i][j][k] = 0;
+        }
+    }
+}
+
+//Loop through list of names to generate probabilities from
+for (until end of file) {
+    var currentName = "John";
+    var a = "J", b = "o", c = "h";
+    var x=0;
+
+    //Read name from list of names
+    //this needs to be added still
+
+    //Add 1 to index of starting letter
+    a = currentName.charAt(x);
+    firstLetters[(a.charCodeAt(0)-97)] += 1;
+
+    //Add 1 to index of starting pair
+    b = currentName.charAt(x+1);
+    doubleLetters[(a.charCodeAt(0)-97)][(b.charCodeAt(0)-97)] += 1;
+
+    //Loop through sets of three letters starting with the first three, advancing by one, through the last three
+    for (;x < (currentName.length-2); x++) {
+        a = currentName.charAt(x);
+        b = currentName.charAt(x+1);
+        c = currentName.charAt(x+2);
+        
+        //Add 1 to index of three letter combo
+        tripleLetters[(a.charCodeAt(0)-97)][(b.charCodeAt(0)-97)][(c.charCodeAt(0)-97)] += 1;
+    }
+    
+    //Add 1 to index of the final two characters and a terminating character
+    a = currentName.charAt(x);
+    b = currentName.charAt(x+1);
+
+    tripleLetters[(a.charCodeAt(0)-97)][(b.charCodeAt(0)-97)][26] += 1;
+
+}
